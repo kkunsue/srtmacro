d9bf19134c14a21fdf53bb88e46db5acd31fca72
louis <kkunsue88@daum.net>
2020-09-26 00:34:51 +0900

    ver. bandai

--- background.js
+++ background.js
@@ -1,38 +1,41 @@
 function playSound() {
-	if (typeof(audio) != "undefined" && audio) {
-		audio.pause();
-		document.body.removeChild(audio);
-		audio = null;
-	}
-	audio = document.createElement('audio');
-	document.body.appendChild(audio);
-	audio.autoplay = true;
-	audio.src = chrome.extension.getURL('assets/tada.mp3');
-	audio.play();
+    if (typeof (audio) != "undefined" && audio) {
+        audio.pause();
+        document.body.removeChild(audio);
+        audio = null;
+    }
+    audio = document.createElement('audio');
+    document.body.appendChild(audio);
+    audio.autoplay = true;
+    audio.src = chrome.extension.getURL('assets/tada.mp3');
+    audio.play();
 }
 
-function sendTelegramMessage() {
-	var botToken = localStorage['botToken'];
-	var chatId = localStorage['chatId'];
-	var msg = encodeURI('Macro has been stopped. Please check your reservation status.');
-	if (botToken != undefined && chatId != undefined) {
-		var url = 'https://api.telegram.org/bot' + botToken + '/sendmessage?chat_id=' + chatId + '&text=' + msg;
-		
-		var xmlhttp = new XMLHttpRequest();
-		xmlhttp.onreadystatechange=function() {
-			if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-				var response = xmlhttp.responseText; //if you need to do something with the returned value
-			}
-		}
-		xmlhttp.open('GET', url, true);
-		xmlhttp.send();
-	}
+function sendTelegram() {
+    var botToken = localStorage['botToken'];
+    var chatId = localStorage['chatId'];
+    var msg = encodeURI('Macro has been stopped. Please check your reservation status.');
+    if (botToken != undefined && chatId != undefined) {
+        var url = 'https://api.telegram.org/bot' + botToken + '/sendmessage?chat_id=' + chatId + '&text=' + msg;
+
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                var response = xmlhttp.responseText; //if you need to do something with the returned value
+            }
+        }
+        xmlhttp.open('GET', url, true);
+        xmlhttp.send();
+    }
 }
 
-chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message && message.type == 'playSound') {
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if (message && message.type === 'got') {
         playSound();
-		sendTelegramMessage();
-        sendResponse(true);
+        setTimeout(function () {
+            alert("New Item found!!! count : " + message.count + "\n\n" + message.names.join("\n"));
+        }, 1000)
+
+        sendResponse();
     }
 });
--- content.js
+++ content.js
@@ -1,130 +1,92 @@
-function injectJs(srcFile) {
-    var scr = document.createElement('script');
-    scr.src = srcFile;
-    document.getElementsByTagName('head')[0].appendChild(scr);
-}
+$(document).ready(function () {
 
-var dsturl1 = "https://etk.srail.co.kr/hpg/hra/01/selectScheduleList.do?pageId=TK0101010000";
+    const isRunning = "on" === sessionStorage.getItem('macro.status');
 
-if (document.URL.substring(0, dsturl1.length) == dsturl1) {
+    if (!isRunning) {
+        helpers.createStartTrigger().click(macro.start).appendTo(helpers.getParentOfTriggers());
+        return;
+    }
 
-	$(document).ready(function() {
-		injectJs(chrome.extension.getURL('inject.js'));
+    helpers.createStopTrigger().click(macro.stop).appendTo(helpers.getParentOfTriggers());
+    helpers.markIdOnCurrentItems();
 
-		var coachSelected = JSON.parse(sessionStorage.getItem('coachSelected'));
-		var firstSelected = JSON.parse(sessionStorage.getItem('firstSelected'));
-		if (coachSelected == null) coachSelected = [];
-		if (firstSelected == null) firstSelected = [];
-		console.log("coach:" + coachSelected);
-		console.log("first:" + firstSelected);
+    const currentItemIds = helpers.getCurrentItemIds();
+    const existsItemIds = sessionStorage.getItem('macro.exists').split(",");
 
-		if (sessionStorage.getItem('macro') == "true") {
-			$("div.button").append('<a href="#" onclick="macrostop();" style="margin-left:5px;"><img src="' + chrome.extension.getURL('images/btn_stop.png') + '"></a>');
-		} else {
-			$("div.button").append('<a href="#" onclick="macro();" style="margin-left:5px;"><img src="' + chrome.extension.getURL('images/btn_start.png') + '"></a>');
-		}
+    const difference = currentItemIds.filter(x => !existsItemIds.includes(x));
 
-		$("<style>")
-    .prop("type", "text/css")
-    .html("\
-    .search-form form .button input, .search-form form .button a img{\
-    	vertical-align: middle;\
-    }")
-    .appendTo("body");
+    if (difference.length > 0) {
+        sessionStorage.removeItem('macro.status');
+        sessionStorage.removeItem('macro.keyword');
+        sessionStorage.removeItem('macro.exists');
 
-		// Inserts the macro button into the table.
-		if ($("#search-list").length != 0) {
-			var rows = $('#search-list table tr');
-			for (i = 1; i < rows.length; i++) {
-				var columns = $(rows[i]).children('td');
-				var first = $(columns[5]);
-				var coach = $(columns[6]);
-				if (coach.children().length > 0) {
-					coach.append($("<p class='p5'></p>"));
-					var checkbox = $("<label></label>").html('<input type="checkbox" name="checkbox" class="coachMacro" value="' + i + '"> 매크로');
-					checkbox.children('input').prop('checked', coachSelected.indexOf(i+"") > -1);
-					coach.append(checkbox);
-				}
-				if (first.children().length > 0) {
-					first.append($("<p class='p5'></p>"));
-					var checkbox = $("<label></label>").html('<input type="checkbox" name="checkbox" class="firstMacro" value="' + i + '"> 매크로');
-					checkbox.children('input').prop('checked', firstSelected.indexOf(i+"") > -1);
-					first.append(checkbox);
-				}
-			}
-		}
+        const message = {
+            type: 'got',
+            names: $(difference).map(helpers.getItemName).get(),
+            count: difference.length
+        };
 
-		if (sessionStorage.getItem('macro') == "true") {
-			// Restores user preferences
-			$("#psgInfoPerPrnb1").val(sessionStorage.getItem('psgInfoPerPrnb1'));
-			$("#psgInfoPerPrnb5").val(sessionStorage.getItem('psgInfoPerPrnb5'));
-			$("#psgInfoPerPrnb4").val(sessionStorage.getItem('psgInfoPerPrnb4'));
-			$("#psgInfoPerPrnb2").val(sessionStorage.getItem('psgInfoPerPrnb2'));
-			$("#psgInfoPerPrnb3").val(sessionStorage.getItem('psgInfoPerPrnb3'));
-			$("#locSeatAttCd1").val(sessionStorage.getItem('locSeatAttCd1'));
-			$("#rqSeatAttCd1").val(sessionStorage.getItem('rqSeatAttCd1'));
+        chrome.extension.sendMessage(message, $.noop);
+    } else {
+        setTimeout(function () {
+            location.reload();
+        }, 20000);
+    }
+});
 
-			if ($("#search-list").length != 0) {
-				var rows = $('#search-list table tr');
+const helpers = (function () {
 
-				var succeed = false;
-				for (i = 1; i < rows.length; i++) {
-					var columns = $(rows[i]).children('td');
+    const extractIdFromElement = function () {
+        return extractId(this.href);
+    }
 
-					var first = $(columns[5]);
-					var coach = $(columns[6]);
+    const extractId = function extractId(str) {
+        return str.match(/goods_code=(\d+)/)[1];
+    }
 
-					if (coachSelected.indexOf(i+"") > -1) {
-						var coachSpecials = coach.children("a");
-						if (coachSpecials.length != 0) {
-							for (j = 0; j < coachSpecials.length; j++) {
-								name = $(coachSpecials[j]).attr('class');
-								if (name == 'button button-02') {
-									$(coachSpecials[0])[0].click();
-									succeed = true;
-									break;
-								}
-							}
-							if (succeed == true) break;
-						}
-					}
+    return {
+        createStartTrigger: function () {
+            return $('<a href="#" style="margin-left:5px;"><img src="' + chrome.extension.getURL('images/btn_start.png') + '" alt="start"></a>');
+        },
+        createStopTrigger: function () {
+            return $('<a href="#" style="margin-left:5px;"><img src="' + chrome.extension.getURL('images/btn_stop.png') + '" alt="stop"></a>');
+        },
+        getParentOfTriggers: function () {
+            return ".reserve_ban_rt";
+        },
+        getCurrentItemIds: function () {
+            return $(".prodct_info > a").map(extractIdFromElement).get();
+        },
+        markIdOnCurrentItems: function () {
+            $(".prodct_info > a").each(function () {
+                $(this).attr("id", extractId(this.href));
+            })
+        },
+        getItemName: function () {
+            const id = this;
+            return $("a[id=" + id + "]").find(".tama_txt01").text();
+        }
+    }
+})();
 
-					if (firstSelected.indexOf(i+"") > -1) {
-						var firstSpecials = first.children("a");
-						if (firstSpecials.length != 0) {
-							for (j = 0; j < firstSpecials.length; j++) {
-								name = $(firstSpecials[j]).attr('class');
-								if (name == 'button button-02') {
-									$(firstSpecials[0])[0].click();
-									succeed = true;
-									break;
-								}
-							}
-							if (succeed == true) break;
-						}
-					}
-				}
+const macro = (function () {
 
-				if (succeed == true) {
-					sessionStorage.removeItem('macro');
-					sessionStorage.removeItem('coachSelected');
-					sessionStorage.removeItem('firstSelected');
-					sessionStorage.removeItem('psgInfoPerPrnb1');
-					sessionStorage.removeItem('psgInfoPerPrnb5');
-					sessionStorage.removeItem('psgInfoPerPrnb4');
-					sessionStorage.removeItem('psgInfoPerPrnb2');
-					sessionStorage.removeItem('psgInfoPerPrnb3');
-					sessionStorage.removeItem('locSeatAttCd1');
-					sessionStorage.removeItem('rqSeatAttCd1');
-					chrome.extension.sendMessage({type: 'playSound'}, function(data) { });
-				} else {
-					setTimeout(function() { 
-					location.reload();
-					}, 1000);
-				}
-			} else {
-				history.go(-1);
-			}
-		}
-	});
-}
+    return {
+        start: function (keyword) {
+            sessionStorage.setItem('macro.status', "on");
+            sessionStorage.setItem("macro.keyword", $.trim(keyword));
+            const currentItemIds = helpers.getCurrentItemIds();
+            sessionStorage.setItem("macro.exists", currentItemIds.join(","))
+
+            location.reload();
+        },
+        stop: function () {
+            alert("매크로를 중지합니다.");
+            sessionStorage.removeItem('macro.status');
+            sessionStorage.removeItem("macro.keyword");
+            sessionStorage.removeItem("macro.exists");
+
+            location.reload();
+        }
+    }
+})();
